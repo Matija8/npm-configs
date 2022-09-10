79b0bdc55324397fcabc253ec2d815d4fdb1b50f
matija8 <matijanme@gmail.com>
2022-09-10 17:59:11 +0200

    feat: test try

---
+++ test-install-packages/try.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# coding: UTF-8
+
+# https://classic.yarnpkg.com/en/docs/cli/info
+yarn info https://npm.pkg.github.com/@matija8/npm-configs
+yarn info https://npm.pkg.github.com/@matija8/npm-config
+
+# Should fail, bad tarball
+yarn add -D https://npm.pkg.github.com/@matija8/npm-configs
+# Should 404
+yarn add -D https://npm.pkg.github.com/@matija8/npm-configs
